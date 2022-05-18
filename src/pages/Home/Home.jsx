eb60d03992fce3ce7ff388c13e587da1786279ec
kirmann <k.dkle@hotmail.fr>
2022-05-18 15:48:12 +0200

    début de création

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./home.scss";
+
+const Home = () => {
+    return (
+        <main className="mainHomeContainer">
+            <div className="backgroundContainer">
+                <div className="titleContainer">
+                    <h1 className="homeTitle">Kirmann Denis</h1>
+                    <p>Developpeur Front-End React</p>
+                </div>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo incidunt consectetur perspiciatis similique magni
+                architecto maiores neque quasi. Enim ipsa possimus sunt quisquam
+                in quam quibusdam praesentium sed, tenetur amet vel ipsam nemo,
+                pariatur quae soluta quas magni, fugiat sapiente sequi! Beatae
+                corporis ab voluptates rem, nam at officia reprehenderit?
+            </p>
+        </main>
+    );
+};
+
+export default Home;

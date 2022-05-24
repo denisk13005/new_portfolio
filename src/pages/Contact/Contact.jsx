9c0cfc4a667ef0c40a0c2cbdd07bdd1a6f8c7cc9
kirmann <k.dkle@hotmail.fr>
2022-05-24 20:39:12 +0200

    bandeau contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./contact.scss";
 
 const Contact = () => {
-    return <section className="contactContainer ">test</section>;
+    return (
+        <section className="contactContainer ">
+            <div className="headBandContainer">
+                <div className="headBand">
+                    <p>dk Développement</p>{" "}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Contact;
